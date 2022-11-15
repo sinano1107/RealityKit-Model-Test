1ab8fe9de2a1cf986d7bb51da2b1be4ff298cb32
sinano1107 <tftf161107@icloud.com>
2022-11-15 18:47:03 +0900

    [update] 飛行機を表示する

--- ModelTest/ViewController.swift
+++ ModelTest/ViewController.swift
@@ -15,10 +15,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // 1. Load model
+        let entity = try! ModelEntity.load(named: "toy_biplane")
+        let parentEntity = ModelEntity()
+        parentEntity.addChild(entity)
+
+        // 2. Create anchor
+        // minimumBounds: 反応するplaneの最小サイズ
+        let anchor = AnchorEntity.init(.plane(.horizontal, classification: .any, minimumBounds: [0, 0]))
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // 3. Place anchor in the scene
+        arView.scene.addAnchor(anchor)
+        
+        // 4. Add entity to anchor
+        anchor.addChild(parentEntity)
+        
+        // 5. Enable gestures
+        // recursive: すべての子孫の衝突形状も生成するかどうか
+        parentEntity.generateCollisionShapes(recursive: true)
+        arView.installGestures(for: parentEntity)
     }
 }
